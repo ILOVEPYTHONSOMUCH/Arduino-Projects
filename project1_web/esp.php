4a6680e90da2ab6edf434fce6c9be45e20b96d0c
SomeOneWannaHackYou <95560177+ILOVEPYTHONSOMUCH@users.noreply.github.com>
2022-06-25 22:38:45 +0700

    Update esp.php

--- project1_web/esp.php
+++ project1_web/esp.php
@@ -7,10 +7,6 @@
     date_default_timezone_set('Asia/Bangkok');
     $time = date('d-m-y h:i:s');
     $conn = mysqli_connect("localhost","root","","esp32");
-    if ($_SESSION['round'] == 20){
-        $sql2 = "DELETE FROM `dht11`";
-        $execute2 = mysqli_query($conn, $sql2);
-    }
     $sql = "INSERT INTO dht11 (hum, tem, time) VALUES ('$hum', '$tem', '$time')";
     $execute = mysqli_query($conn, $sql);
  }
